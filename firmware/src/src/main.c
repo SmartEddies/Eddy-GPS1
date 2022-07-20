fc57822695a0383e8390884f1594a7c6d7eeb691
fbalestrini <gaojing@hotmail.it>
2022-07-20 09:24:25 +0200

    Updated

--- firmware/src/src/main.c
+++ firmware/src/src/main.c
@@ -1,12 +1,12 @@
 #include "mgos.h"
-#include "mgos_gps.h"
+#include "mgos_bgps.h"
 
 static void mg_gps_position_changed(int ev, void *ev_data, void *userdata) {
-  struct mgos_bgps_position_changed *pos_changed = (struct mgos_bgps_position_changed *)ev_data;
+  struct mgos_bgps_position_changed *data = (struct mgos_bgps_position_changed *)ev_data;
   LOG(LL_INFO, ("GPS: lat %3f, lng %3f (accuracy %2f)",
-    pos_changed->cur_pos.location.latitude,
-    pos_changed->cur_pos.location.longitude,
-    pos_changed->cur_pos.accuracy));
+    data->cur_pos.location.latitude,
+    data->cur_pos.location.longitude,
+    data->cur_pos.accuracy));
 
   (void) ev;
   (void) userdata;
