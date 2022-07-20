65e0d88741ed37cea76e42498d0352fcd890bbbe
fbalestrini <gaojing@hotmail.it>
2022-07-20 09:23:00 +0200

    Updated

--- firmware/src/src/main.c
+++ firmware/src/src/main.c
@@ -1,6 +1,18 @@
 #include "mgos.h"
+#include "mgos_gps.h"
+
+static void mg_gps_position_changed(int ev, void *ev_data, void *userdata) {
+  struct mgos_bgps_position_changed *pos_changed = (struct mgos_bgps_position_changed *)ev_data;
+  LOG(LL_INFO, ("GPS: lat %3f, lng %3f (accuracy %2f)",
+    pos_changed->cur_pos.location.latitude,
+    pos_changed->cur_pos.location.longitude,
+    pos_changed->cur_pos.accuracy));
+
+  (void) ev;
+  (void) userdata;
+}
 
 enum mgos_app_init_result mgos_app_init(void) {
-
+  mgos_event_add_handler(MGOS_EV_BGPS_POSITION_CHANGED, mg_gps_position_changed, NULL);
   return MGOS_APP_INIT_SUCCESS;
 }
